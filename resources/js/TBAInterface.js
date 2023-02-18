4e5489d1d848c81b83b84fcf5633e15aa88eb1b8
btmartin <ben@techfire225.org>
2023-02-18 15:38:45 -0600

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "ih1nhDJZ55u5KHSvzDvR8wsN8cwItSQya0LihbYTmBpY77nG91AD4rDIEENhqaYu";
 /**
  * Get list of teams in event
  *
