d95a34d1f7e404d3d66b296c43d9f78ab9c5cb3e
btmartin <ben@techfire225.org>
2023-03-27 22:02:05 -0500

    Update CU_config_pit.js

--- 2023/CU_config_pit.js
+++ 2023/CU_config_pit.js
@@ -3,7 +3,7 @@ var config_data = `
   "title": "2481 Scouting",
   "page_title": "2023 7 Rivers",
   "checkboxAs": "10",
-  "prematch": [
+  "pit": [
     { "name": "Scouter",
       "code": "s",
       "gsCol": "scouter",
@@ -68,170 +68,54 @@ var config_data = `
       "filename": "2023/field_image.png",
       "clickRestriction": "one",
       "shape": "circle 5 black red true"
-    }
-  ],
-  "auton": [
-    { "name": "Auto Scoring",
-      "code": "asg",
-      "type": "clickable_image",
-      "filename": "2023/grid_image_2.png",
-      "dimensions": "9 4",
-      "clickRestriction": "onePerBox",
-      "toggleClick": "true",
-      "showFlip": "false",
-      "showUndo": "false",
-      "shape": "circle 12 black red true"
     },
-    { "name": "Mobility Bonus?",
-      "code": "am",
-      "type": "bool"
+    { "name": "Weight With Bumpers",
+      "code": "wwb",
+      "type": "text",
+      "size": 25,
+      "maxSize": 1000
     },
-    { "name": "Charge Station",
-      "code": "ad",
-      "type":"radio",
-      "choices": {
-        "d": "Docked (not Engaged)<br>",
-        "e": "Engaged<br>",
-        "a": "Attempted but failed<br>",
-        "x": "Not attempted"
-      },
-      "defaultValue": "x"
+    { "name": "Long Dimension",
+      "code": "ld",
+      "type": "text",
+      "size": 25,
+      "maxSize": 1000
     },
-    { "name": "Auto Type (relative to driver station)",
-      "code": "at",
-      "type":"radio",
-      "choices": {
-        "b": "Bump Side<br>",
-        "n": "No Bump Side<br>",
-        "c": "Center<br>",
-        "o": "Only Scored, No Movement<br>",
-        "x": "Did Nothing"
-      },
-      "defaultValue": "x"
-    }
-  ],
-  "teleop": [
-    { "name": "Grid Scoring",
-      "code": "tsg",
-      "type": "clickable_image",
-      "filename": "2023/grid_image_2.png",
-      "dimensions": "9 4",
-      "clickRestriction": "onePerBox",
-      "toggleClick": "true",
-      "showFlip": "false",
-      "showUndo": "false",
-      "shape": "circle 12 black red true"
-    },
-    { "name": "Successful Cycles",
-      "code": "sc",
-      "type": "counter"
-    },
-    { "name": "Missed Cycles",
-      "code": "mc",
-      "type": "counter"
-    },
-    { "name": "Was Defended",
+    { "name": "Wide Dimension",
       "code": "wd",
-      "type": "bool"
-    },
-    { "name": "Who Defended this bot",
-      "code": "who",
-      "type": "text"
-    },
-    { "name": "Fouls",
-      "code": "ls",
-      "type": "counter"
-    },
-    { "name": "Cycling - Single Substation",
-      "code": "css",
-      "type": "bool"
-    },
-    { "name": "Cycling - Double Substation Shelf",
-      "code": "cdss",
-      "type": "bool"
-    },
-    { "name": "Cycling - Double Substation Floor",
-      "code": "cdsf",
-      "type": "bool"
-    },
-    { "name": "Cycling - Floor",
-      "code": "cf",
-      "type": "bool"
-    },
-    { "name": "No Cycling",
-      "code": "nc",
-      "type": "bool"
-    }
-  ],
-  "endgame": [
-    { "name": "Final Status",
-      "code": "fs",
-      "type":"radio",
-      "choices": {
-        "p": "Parked<br>",
-        "d": "Docked (Not Engaged)<br>",
-        "e": "Engaged<br>",
-        "x": "No points"
-      },
-      "defaultValue": "x"
-    }
-  ],
-  "postmatch": [
-    { "name": "Defense Rating",
-      "code": "dr",
-      "type": "radio",
-      "choices": {
-        "b": "Below Average<br>",
-        "a": "Average<br>",
-        "g": "Good<br>",
-        "e": "Excellent<br>",
-        "x": "Did not play defense"
-      },
-      "defaultValue": "x"
-    },
-    { "name": "Breakage / Connectivity Issues",
-      "code": "die",
-      "type": "bool"
-    },
-    { "name": "Tipped",
-      "code": "tip",
-      "type": "bool"
-    },
-    { "name": "Comments: Match Flow Issues",
-      "code": "coa",
       "type": "text",
       "size": 25,
       "maxSize": 1000
     },
-    { "name": "Comments: Tippiness",
-      "code": "cot",
+    { "name": "What autos can they do",
+      "code": "au",
       "type": "text",
       "size": 25,
       "maxSize": 1000
     },
-    { "name": "Comments: Fouls",
-      "code": "cof",
-      "type": "text",
-      "size": 25,
-      "maxSize": 1000
-    },
-    { "name": "Comments: Balancing Details",
+    { "name": "Comments: Robot Build Quality",
       "code": "cob",
       "type": "text",
       "size": 25,
       "maxSize": 1000
     },
-    { "name": "Comments: Defense Details",
-      "code": "coc",
+    { "name": "Other Comments",
+      "code": "coo",
       "type": "text",
       "size": 25,
       "maxSize": 1000
     },
-    { "name": "Comments: Other",
-      "code": "cod",
-      "type": "text",
-      "size": 25,
-      "maxSize": 1000
+    { "name": "Drivetrain Type",
+      "code": "dt",
+      "type":"radio",
+      "choices": {
+        "s": "Swerve<br>",
+        "t": "Tank<br>",
+        "m": "Mecanum<br>",
+        "o": "Omni"
+        "r": "Other"
+      },
+      "defaultValue": "s"
     }
   ]
 }`;
