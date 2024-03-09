77e7cf80d7ed6f7fe68d9a8bf11f2bc471a55c62
btmartin <ben@techfire225.org>
2024-03-08 20:48:00 -0600

    Update CU_config_pit.js

--- 2023/CU_config_pit.js
+++ 2023/CU_config_pit.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "title": "2481 Scouting - Pit",
-  "page_title": "2023 Worlds - Pit",
+  "page_title": "2024 CIR - Pit",
   "checkboxAs": "10",
   "pit": [
     { "name": "Scouter",
