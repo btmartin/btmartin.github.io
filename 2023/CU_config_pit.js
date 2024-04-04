15798604a1d49bbc3b51bd37608b1429c68aebed
btmartin <ben@techfire225.org>
2024-04-04 15:18:44 -0500

    Update CU_config_pit.js

--- 2023/CU_config_pit.js
+++ 2023/CU_config_pit.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "title": "2481 Scouting - Pit",
-  "page_title": "2024 CIR - Pit",
+  "page_title": "2024 Rocket City - Pit",
   "checkboxAs": "10",
   "pit": [
     { "name": "Scouter",
