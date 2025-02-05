3f91b766a2af546210b0502cd8ae96263a69e296
btmartin <ben@techfire225.org>
2025-02-04 19:53:27 -0600

    Update CU_config.js
    update

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "title": "2481 Match Scouting",
-  "page_title": "2025 Arkansas",
+  "page_title": "2025 Arkansas ",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter",
