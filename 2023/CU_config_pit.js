6ccc4ffa9ed6f9f4649d283832e858e951e5c336
btmartin <ben@techfire225.org>
2025-03-02 20:42:24 -0600

    Update CU_config_pit.js

--- 2023/CU_config_pit.js
+++ 2023/CU_config_pit.js
@@ -43,6 +43,17 @@ var config_data = `
       "size": 25,
       "maxSize": 1000
     },
+    { "name": "Intake Type",
+      "code": "it",
+      "type":"radio",
+      "choices": {
+        "s": "Station<br>",
+        "g": "Ground<br>",
+        "b": "Both<br>",
+        "n": "N/A"
+      },
+      "defaultValue": "n"
+    },
     { "name": "Comments",
       "code": "coo",
       "type": "text",
