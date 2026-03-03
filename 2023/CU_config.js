a39dcfee74f0428276df4aff88025524a6b6b81f
btmartin <ben@techfire225.org>
2026-03-02 19:51:46 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -82,7 +82,7 @@ var config_data = `
     },
     { "name": "Where did they pick up from",
       "code": "am",
-      "type": "radio",
+      "type": "checkbox",
       "choices": {
          "Depot": "Depot<br>",
          "Outpost": "Outpost<br>",
